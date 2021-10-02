c03da94ca66aaae6ee8199b31d114da753d180e2
Rabindra Joshi <mail@rjoshi.net>
2021-10-02 16:25:32 +0545

    dotenv, nodemon, envfile fix

--- config/database.js
+++ config/database.js
@@ -1,13 +1,8 @@
 //Set up mongoose connection
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load();
-}
 const mongoose = require('mongoose');
 const mongoDB = process.env.mongoDB;
-mongoose.connect(
-  mongoDB,
-  { useNewUrlParser: true }
-);
+
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 // deprecated method!
 mongoose.set('useFindAndModify', false);
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load();
-}
+require('dotenv').config();
+
 const port = process.env.PORT;
 
 const express = require('express');
@@ -19,36 +18,28 @@ app.set('views', path.join(__dirname, 'views'));
 
 const mongoose = require('./config/database');
 // connect to mongodb
-mongoose.connection.on(
-  'error',
-  console.error.bind(console, 'MongoDB connection error:')
-);
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(cors());
 app.use(logger('dev'));
 
 app.use('/todos', todos);
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index', { title: 'Proshore', app: 'Todo' });
 });
 
 // handle 404 error
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   console.log(err.status, err.message);
 
-  if (err.status === 404)
-    res.status(404).json({ status: 'error', message: 'Not found' });
-  else if (err.status && err.message)
-    res.status(err.status).json({ status: 'error', message: err.message });
-  else
-    res
-      .status(500)
-      .json({ status: 'error', message: 'Internal error occurred' });
+  if (err.status === 404) res.status(404).json({ status: 'error', message: 'Not found' });
+  else if (err.status && err.message) res.status(err.status).json({ status: 'error', message: err.message });
+  else res.status(500).json({ status: 'error', message: 'Internal error occurred' });
 });
 
 app.listen(port, () => {
